431faf49fdd1167eafee55c4cf62f0f9f2876095
Joseph Kopecky <josephkopecky@jmkopecky.tech>
2023-08-07 08:00:37 -0500

    Added Sculk Planks and the texture for the Ancient Focus

--- src/main/java/dev/prognitio/beyondthevoid/items/BlocksRegistry.java
+++ src/main/java/dev/prognitio/beyondthevoid/items/BlocksRegistry.java
@@ -6,10 +6,13 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Material;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+
+import java.util.function.ToIntFunction;
 
 public class BlocksRegistry {
 
@@ -17,13 +20,22 @@ public class BlocksRegistry {
     public static final DeferredRegister<Item> BLOCKITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, BeyondTheVoid.MODID);
 
 
+    public static ToIntFunction<BlockState> lightLevel15 = BlockState -> 15;
+    public static ToIntFunction<BlockState> lightLevel10 = BlockState -> 10;
+
+
     public static final RegistryObject<Block> VOID_RIFT_CORE = BLOCKS.register("void_rift_core",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).requiresCorrectToolForDrops().strength(5.0f)));
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).requiresCorrectToolForDrops().strength(5.0f).lightLevel(lightLevel15)));
     public static final RegistryObject<Item> VOID_RIFT_CORE_ITEM = BLOCKITEMS.register("void_rift_core_item",
             () -> new BlockItem(VOID_RIFT_CORE.get(), new Item.Properties().tab(CreativeTabs.BEYONDTHEVOIDTAB)));
 
     public static final RegistryObject<Block> VOID_RIFT_PEDESTAL = BLOCKS.register("void_rift_pedestal",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).requiresCorrectToolForDrops().strength(5.0f).noOcclusion()));
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).requiresCorrectToolForDrops().strength(5.0f).noOcclusion().lightLevel(lightLevel10)));
     public static final RegistryObject<Item> VOID_RIFT_PEDESTAL_ITEM = BLOCKITEMS.register("void_rift_pedestal_item",
             () -> new BlockItem(VOID_RIFT_PEDESTAL.get(), new Item.Properties().tab(CreativeTabs.BEYONDTHEVOIDTAB)));
+
+    public static final RegistryObject<Block> SCULK_PLANKS = BLOCKS.register("sculk_planks",
+            () -> new Block(BlockBehaviour.Properties.of(Material.WOOD).strength(2.5f, 3.0f)));
+    public static final RegistryObject<Item> SCULK_PLANKS_ITEM = BLOCKITEMS.register("sculk_planks_item",
+            () -> new BlockItem(SCULK_PLANKS.get(), new Item.Properties().tab(CreativeTabs.BEYONDTHEVOIDTAB)));
 }
